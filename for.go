7544ef5050bbba24705af1a34bd5e82d27837d8d
muhamadhaspin <muhamadhaspin@gmail.com>
2023-12-13 13:52:48 +0700

    learned for loops

---
+++ for.go
@@ -0,0 +1,28 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"strconv"
+// )
+
+// func main() {
+// var counterA = 0
+// for counterA < 10 {
+// 	fmt.Println("A : ", counterA+1)
+// 	counterA++
+// }
+
+// for counterB := 0; counterB < 10; counterB++ {
+// 	fmt.Println("B : ", counterB+1)
+// }
+
+// slice := []string{"Uwais", "Qaswarah", "Yara", "Leen"}
+// for i := 0; i < len(slice); i++ {
+// 	fmt.Println(slice[i])
+// }
+
+// friends := []string{"Othman", "Abdullah", "Saeed", "Mubarak"}
+// for index, name := range friends {
+// 	fmt.Println("No. "+strconv.Itoa(index+1), "- Name : "+name)
+// }
+// }
