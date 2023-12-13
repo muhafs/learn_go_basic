5788027e68b148c51ba978252b072eb4b078eef3
muhamadhaspin <muhamadhaspin@gmail.com>
2023-12-13 13:58:14 +0700

    learned break and continue of loops

---
+++ break.go
@@ -0,0 +1,13 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	for i := 0; i < 10; i++ {
+// 		if i == 5 {
+// 			break // will stop/end the loop if value equals to 5
+// 		}
+
+// 		fmt.Println(i)
+// 	}
+// }
