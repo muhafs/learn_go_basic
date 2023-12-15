25981899fffe41da7c67ab480be8f7f2aa46e4e7
muhamadhaspin <muhamadhaspin@gmail.com>
2023-12-15 10:43:41 +0700

    learned interface

---
+++ interface.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type HasName interface {
+	GetName() string
+}
+
+func SayHi(user HasName) {
+	fmt.Println("Hi, I'm", user.GetName())
+}
+
+type Person struct {
+	Name string
+}
+
+func (prsn Person) GetName() string {
+	return prsn.Name
+}
+
+func main() {
+	fira := Person{"Fira"}
+	SayHi(fira)
+}
