1ec71f9d07d8a919d85c37b164ded6b24ebce844
muhamadhaspin <muhamadhaspin@gmail.com>
2023-12-15 10:43:29 +0700

    learned struct

---
+++ struct.go
@@ -0,0 +1,35 @@
+package main
+
+// import "fmt"
+
+// type User struct {
+// 	Name, Address string
+// 	Age           int
+// }
+
+// func main() {
+// 	// 1
+// 	var uwais User
+// 	uwais.Name = "Uwais"
+// 	uwais.Age = 2
+// 	uwais.Address = "Madura"
+
+// 	fmt.Println(uwais.Name)
+// 	fmt.Println(uwais.Age)
+
+// 	// 2
+// 	yara := User{
+// 		Name:    "Yara",
+// 		Age:     2,
+// 		Address: "Aceh",
+// 	}
+
+// 	fmt.Println(yara.Name)
+// 	fmt.Println(yara.Age)
+
+// 	// 3
+// 	qaswarah := User{"Qaswarah", "Makkah", 2}
+
+// 	fmt.Println(qaswarah.Name)
+// 	fmt.Println(qaswarah.Age)
+// }
---
+++ structMethod.go
@@ -0,0 +1,16 @@
+package main
+
+// import "fmt"
+
+// type Student struct {
+// 	Name string
+// }
+
+// func (std Student) saySomthing() {
+// 	fmt.Println("Hi, my name is", std.Name)
+// }
+
+// func main() {
+// 	otto := Student{"Othman"}
+// 	otto.saySomthing()
+// }
