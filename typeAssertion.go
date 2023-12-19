d35c4ff61759e26c39b9ca2a635f6118dcc0a128
muhamadhaspin <muhamadhaspin@gmail.com>
2023-12-19 20:29:45 +0700

    learned Type Assertion/Cast/As

---
+++ typeAssertion.go
@@ -0,0 +1,22 @@
+package main
+
+// import "fmt"
+
+// func anything() interface{} {
+// 	return 100
+// }
+
+// func main() {
+// 	var res interface{} = anything()
+// 	// var resAsString string = res.(string) // consider (res) as String
+// 	// fmt.Println(resAsString)
+
+// 	switch val := res.(type) {
+// 	case string:
+// 		fmt.Println(val, "is String")
+// 	case int:
+// 		fmt.Println(val, "is Integer")
+// 	default:
+// 		fmt.Println(val, "is Uknown Type")
+// 	}
+// }
