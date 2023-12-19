a2ca1d3b8cfec3862e2b2e0e61d9d81b964860b7
muhamadhaspin <muhamadhaspin@gmail.com>
2023-12-19 21:03:52 +0700

    learned pointer in variables

---
+++ pointer.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	Country, City string
+}
+
+func main() {
+	// houses
+	var guestAd Address = Address{"Saudi Arabia", "Makkah"}
+	var othmanAd Address = Address{"Indonesia", "Malang"}
+	var abdullahAd Address = Address{"Indonesia", "Sampang"}
+
+	// guest in othman house
+	var guest *Address = &othmanAd
+	fmt.Println(othmanAd)
+	fmt.Println(abdullahAd)
+	fmt.Println(guest)
+
+	// guest in abdullah house
+	guest = &abdullahAd
+	fmt.Println(othmanAd)
+	fmt.Println(abdullahAd)
+	fmt.Println(guest)
+
+	// guest brought abdullah into othman house
+	*guest = othmanAd
+	fmt.Println(othmanAd)
+	fmt.Println(abdullahAd)
+	fmt.Println(guest)
+
+	*guest = guestAd
+	fmt.Println(othmanAd)
+	fmt.Println(abdullahAd)
+	fmt.Println(guest)
+}
